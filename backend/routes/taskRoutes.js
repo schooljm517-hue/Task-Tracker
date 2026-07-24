63bc743a3692f3e77e89df9de0a5b86506b6516a
Michael <jmcaluscos02@gmail.com>
2026-07-24 13:39:45 +0800

    feat: implement task and category management APIs

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { getTasks, getTaskById, createTask, updateTask, deleteTask } = require('../controllers/taskController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+const router = express.Router();
+
+router.get('/', authMiddleware, getTasks);
+router.get('/:id', authMiddleware, getTaskById);
+router.post('/', authMiddleware, createTask);
+router.put('/:id', authMiddleware, updateTask);
+router.delete('/:id', authMiddleware, deleteTask);
+
+module.exports = router;
